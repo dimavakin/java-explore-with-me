21f304e297ad537504feeeaeab383e17380927a9
dimavakin <vakindmitri@yandex.ru>
2025-04-10 20:00:44 +0300

    Этап 2. Основной сервис

--- ewm-service/src/main/java/ru/practicum/dto/request/ParticipationRequestDto.java
+++ ewm-service/src/main/java/ru/practicum/dto/request/ParticipationRequestDto.java
@@ -7,15 +7,13 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.enums.RequestStatus;
 
-import java.time.LocalDateTime;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ParticipationRequestDto {
     Long id;
-    LocalDateTime created;
+    String created;
     Long event;
     Long requester;
     RequestStatus status;
--- ewm-service/src/main/java/ru/practicum/mapper/RequestMapper.java
+++ ewm-service/src/main/java/ru/practicum/mapper/RequestMapper.java
@@ -4,6 +4,9 @@ import ru.practicum.exception.ValidationException;
 import ru.practicum.model.Request;
 import ru.practicum.dto.request.ParticipationRequestDto;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class RequestMapper {
     public static ParticipationRequestDto toParticipationRequestDtoFromRequest(Request request) {
         if (request == null) {
@@ -12,10 +15,14 @@ public class RequestMapper {
         ParticipationRequestDto dto = new ParticipationRequestDto();
         dto.setId(request.getId());
         dto.setRequester(request.getRequester().getId());
-        dto.setCreated(request.getCreated());
+        dto.setCreated(formatTimeWithNanoseconds(request.getCreated()));
         dto.setStatus(request.getStatus());
         dto.setEvent(request.getEvent().getId());
         return dto;
     }
 
+    public static String formatTimeWithNanoseconds(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS");
+        return dateTime.format(formatter);
+    }
 }
