f1e5a96ff38d9a4740be7419a5ded3a6de5fc0f7
dimavakin <vakindmitri@yandex.ru>
2025-04-10 20:47:08 +0300

    Этап 2. Основной сервис

--- ewm-service/src/main/java/ru/practicum/dto/event/NewEventDto.java
+++ ewm-service/src/main/java/ru/practicum/dto/event/NewEventDto.java
@@ -5,7 +5,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
